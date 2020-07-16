aec0b94c10565458dbcd23165dd3e16a8f316563
prin-r <q@bandprotocol.com>
2020-07-16 15:38:15 +0700

    -

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! Delphi Oracle Service
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs, rust_2018_idioms, trivial_casts, unused_qualifications)]
+
+pub mod application;
+pub mod commands;
+pub mod config;
+pub mod error;
+pub mod networks;
+pub mod prelude;
+pub mod sources;
